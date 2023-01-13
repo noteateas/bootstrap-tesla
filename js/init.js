020c545a80ccb3ef073bfb38c1e26ca1a8f8d17a
noteateas <45702543+noteateas@users.noreply.github.com>
2023-01-13 17:08:43 +0100

    first commit

---
+++ js/init.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+$(document).ready(function(){
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+});
+$(document).ready(function(){
+	$('.button-collapse').sideNav();
+});
+
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+
+$(document).ready(function(){
+    $('select').formSelect();
+  });
